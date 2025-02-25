b436ff9712ac2358321f94ba315ac0770e7cec5c
Reimanbow <reima@cps.im.dendai.ac.jp>
2025-02-25 16:06:12 +0900

    fixed: 詳細を記述

--- cdev/ioctl-2/ioctl-2.c
+++ cdev/ioctl-2/ioctl-2.c
@@ -1,5 +1,7 @@
 /**
  * @file ioctl-2.c
+ * 
+ * キャラクタデバイスドライバを実装して, ioctlを用いたメッセージのやり取りができる
  */
 #include <linux/atomic.h>
 #include <linux/cdev.h>
@@ -21,15 +23,21 @@
 #define SUCCESS 0
 #define BUF_LEN 80
 
+/**
+ * @enum 排他制御のための列挙体
+ */
 enum {
 	CDEV_NOT_USED,
 	CDEV_EXCLUSIVE_OPEN,
 };
 
+//! デバイスの排他制御を行う(初期状態は未使用)
 static atomic_t already_open = ATOMIC_INIT(CDEV_NOT_USED);
 
+//! デバイス内のメッセージを格納するバッファ
 static char message[BUF_LEN + 1];
 
+//! デバイスのクラスに使用
 static struct class *cls;
 
 /**
@@ -38,6 +46,7 @@ static struct class *cls;
 static int device_open(struct inode *inode, struct file *file) {
 	pr_info("device_open(%p)\n", file);
 
+	/* カーネルモジュールの参照カウントを増やす. モジュールがアンロードされないようにする */
 	try_module_get(THIS_MODULE);
 
 	return SUCCESS;
@@ -49,6 +58,7 @@ static int device_open(struct inode *inode, struct file *file) {
 static int device_release(struct inode *inode, struct file *file) {
 	pr_info("device_release(%p,%p)\n", inode, file);
 	
+	/* カーネルモジュールの参照カウントを減らす. アンロードを可能にする */
 	module_put(THIS_MODULE);
 
 	return SUCCESS;
@@ -63,8 +73,14 @@ static ssize_t device_read(struct file *file, char __user *buffer,
 	/* バッファに書き込まれたバイト数 */
 	int bytes_read = 0;
 
+	/* messageのポインタを取得する */
 	const char *message_ptr = message;
 
+	/**
+	 * データの終端チェック
+	 * *offset位置のデータがNULLなら, 読み取り終了(0を返す)
+	 * *offset = 0; でオフセットをリセット
+	 */
 	if (!*(message_ptr + *offset)) {
 		*offset = 0;
 		return 0;
@@ -72,7 +88,10 @@ static ssize_t device_read(struct file *file, char __user *buffer,
 
 	message_ptr += *offset;
 
-	/* データをバッファに入れる */
+	/**
+	 * offset位置からbufferへデータをコピー
+	 * lengthバイト or messageの終端まで繰り返し
+	 */
 	while (length && *message_ptr) {
 		/* put_userでユーザ空間へ1バイトずつコピーする */
 		put_user(*(message_ptr++), buffer++);
@@ -82,6 +101,7 @@ static ssize_t device_read(struct file *file, char __user *buffer,
 
 	pr_info("Read %d bytes, %ld left\n", bytes_read, length);
 
+	/* オフセットを更新 */
 	*offset += bytes_read;
 
 	return bytes_read;
@@ -97,7 +117,9 @@ static ssize_t device_write(struct file *file, const char __user *buffer,
 
 	pr_info("device_write(%p,%p,%ld)", file, buffer, length);
 
+	/* ユーザ空間のbufferからmessageにデータをコピー */
 	for (i = 0; i < length && i < BUF_LEN; i++) {
+		/* ユーザ空間のデータをカーネル空間へコピー */
 		get_user(message[i], buffer + i);
 	}
 
@@ -105,47 +127,72 @@ static ssize_t device_write(struct file *file, const char __user *buffer,
 }
 
 /**
- * @brief ioctl()が呼び出されたときの処理
+ * @brief ioctl()が呼び出されたときの処理. 通常のread()やwrite()ではできないデバイス固有の操作を実現できる
+ * 
+ * @param file file構造体
+ * @param ioctl_num ioctlのコマンド番号(ioctl-2.hで定義)
+ * @param ioctl_param ioctlの引数(ユーザ空間から渡されるデータ)
  */
 static long device_ioctl(struct file *file, unsigned int ioctl_num, unsigned long ioctl_param) {
 	int i;
 	long ret = SUCCESS;
 
+	/* 排他制御: デバイスが既にオープンされていないかチェック */
 	if (atomic_cmpxchg(&already_open, CDEV_NOT_USED, CDEV_EXCLUSIVE_OPEN)) {
 		return -EBUSY;
 	}
 
+	/* 受け取ったioctl_numに応じて, 異なる動作を実行する */
 	switch (ioctl_num) {
+	/* メッセージを設定 */
 	case IOCTL_SET_MSG: {
+		/* ioctl_paramはユーザ空間の文字列のポインタであるのでキャストする(char __user *tmp) */
 		char __user *tmp = (char __user *)ioctl_param;
 		char ch;
 
+		/* tmpの最初の文字をカーネル空間へのコピー */
 		get_user(ch, tmp);
+		/* BUF_LENまでの文字列を取得 */
 		for (i = 0; ch && i < BUF_LEN; i++, tmp++) {
 			get_user(ch, tmp);
 		}
 
+		/* 取得したデータをmessageに格納する */
 		device_write(file, (char __user *)ioctl_param, i, NULL);
 		break;
 	}
+	/* メッセージを取得 */
 	case IOCTL_GET_MSG: {
 		loff_t offset = 0;
 
+		/**
+		 * ioctl_paramは, ユーザ空間のバッファのポインタであるのでキャストする
+		 * device_read()を呼び出して, messageの内容をioctl_paramにコピー
+		 */
 		i = device_read(file, (char __user *)ioctl_param, 99, &offset);
 
+		/* 取得したデータの最後に'\0'を追加する */
 		put_user('\0', (char __user *)ioctl_param + i);
 		break;
 	}
+	/* メッセージのnバイト目を取得 */
 	case IOCTL_GET_NTH_BYTE:
+		/* ioctl_paramは, 取得したいバイト位置(n) */
+		/* message[n]の値をlong型で返す */
 		ret = (long)message[ioctl_param];
 		break;
 	}
 
+	/* デバイスの使用状態をCDEV_NOT_USEDに戻す */
 	atomic_set(&already_open, CDEV_NOT_USED);
 
 	return ret;
 }
 
+/**
+ * @struct file_operations
+ * @brief キャラクタデバイスが対応する操作を定義する構造体
+ */
 static struct file_operations fops = {
 	.read = device_read,
 	.write = device_write,
@@ -158,6 +205,7 @@ static struct file_operations fops = {
  * @brief カーネルモジュール初期化関数
  */
 static int __init chardev_init(void) {
+	/* キャラクタデバイスを登録する */
 	int ret_val = register_chrdev(MAJOR_NUM, DEVICE_FILE_NAME, &fops);
 
 	if (ret_val < 0) {
@@ -166,12 +214,14 @@ static int __init chardev_init(void) {
 		return ret_val;
 	}
 
+	/* デバイスクラスの作成 */
 #if LINUX_VERSION_CODE >= KERNEL_VERSION(6, 4, 0)
 	cls = class_create(DEVICE_FILE_NAME);
 #else
 	cls = class_create(THIS_MODULE, DEVICE_FILE_NAME);
 #endif
 
+	/* /dev/char_devというデバイスノードを作成するs */
 	device_create(cls, NULL, MKDEV(MAJOR_NUM, 0), NULL, DEVICE_FILE_NAME);
 
 	pr_info("Device created on /dev/%s\n", DEVICE_FILE_NAME);
@@ -183,9 +233,13 @@ static int __init chardev_init(void) {
  * @brief カーネルモジュールクリーンアップ関数
  */
 static void __exit chardev_exit(void) {
+	/* /dev/char_devを削除 */
 	device_destroy(cls, MKDEV(MAJOR_NUM, 0));
+
+	/* 作成したデバイスクラスを削除 */
 	class_destroy(cls);
 
+	/* 登録したデバイスをカーネルから削除 */
 	unregister_chrdev(MAJOR_NUM, DEVICE_FILE_NAME);
 }
 
--- cdev/ioctl-2/userspace-ioctl.c
+++ cdev/ioctl-2/userspace-ioctl.c
@@ -1,7 +1,9 @@
 /**
  * @file userspace-ioctl.c
+ * 
+ * ioctlを使ってカーネルのデバイスドライバにデータを送受信することができる
  */
-#include "ioctl-2.h"
+#include "ioctl-2.h"	/* ioctlにIOCTL_SET_MSGなどの#defineを取得 */
 
 #include <stdio.h>
 #include <fcntl.h>
@@ -9,9 +11,13 @@
 #include <stdlib.h>
 #include <sys/ioctl.h>
 
+/**
+ * @brief メッセージをデバイスに送る
+ */
 int ioctl_set_msg(int file_desc, char *message) {
 	int ret_val;
 
+	/* IOCTL_SET_MSGを呼び出し, デバイスにメッセージを送る */
 	ret_val = ioctl(file_desc, IOCTL_SET_MSG, message);
 
 	if (ret_val < 0) {
@@ -21,26 +27,35 @@ int ioctl_set_msg(int file_desc, char *message) {
 	return ret_val;
 }
 
+/**
+ * @brief デバイスからメッセージを取得
+ */
 int ioctl_get_msg(int file_desc) {
 	int ret_val;
 	char message[100] = {0};
 
+	/* IOCTL_GET_MSGを呼び出し, デバイスからメッセージを取得 */
 	ret_val = ioctl(file_desc, IOCTL_GET_MSG, message);
 
 	if (ret_val < 0) {
 		printf("ioctl_get_msg failed:%d\n", ret_val);
 	}
+	/* 取得したメッセージをprintfで表示 */
 	printf("get_msg message:%s", message);
 
 	return ret_val;
 }
 
+/**
+ * @brief メッセージをiバイトずつ取得
+ */
 int ioctl_get_nth_byte(int file_desc) {
 	int i, c;
 
 	printf("get_nth_byte message:");
 
 	i = 0;
+	/* 1バイトずつデバイスから取得する */
 	do {
 		c = ioctl(file_desc, IOCTL_GET_NTH_BYTE, i++);
 
@@ -49,30 +64,42 @@ int ioctl_get_nth_byte(int file_desc) {
 			return c;
 		}
 
+		/* 取得したバイトを表示 */
 		putchar(c);
 	} while (c != 0);
 
 	return 0;
 }
 
+/**
+ * @brief プログラムのエントリポイント
+ */
 int main(void) {
+	/* デバイスファイルのファイルディスクリプタ */
 	int file_desc, ret_val;
+	/* デバイスに送るメッセージ */
 	char *msg = "Message passed by ioctl\n";
 
+	/* デバイスファイルを開く */
 	file_desc = open(DEVICE_PATH, O_RDWR);
 	if (file_desc < 0) {
 		printf("Can't open device file: %s, error: %d\n", DEVICE_PATH, file_desc);
 		exit(EXIT_FAILURE);
 	}
 
+	/* メッセージを書き込む */
 	ret_val = ioctl_set_msg(file_desc, msg);
 	if (ret_val) {
 		goto error;
 	}
+
+	/* 1バイトずつ取得する */
 	ret_val = ioctl_get_nth_byte(file_desc);
 	if (ret_val) {
 		goto error;
 	}
+
+	/* メッセージ全体を取得 */
 	ret_val = ioctl_get_msg(file_desc);
 	if (ret_val) {
 		goto error;
